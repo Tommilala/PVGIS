f7984b4c7cdee1eff8543e4017e042ed2d04906a
Tommi Anttila <tommi.anttilala@gmail.com>
2020-09-02 23:56:16 +0300

    Fetch PVGIS data for sampled points in shapes

--- main.py
+++ main.py
@@ -1,28 +1,58 @@
-import requests
+import geopandas as gpd
 import pandas as pd
+import matplotlib.pyplot as plt
+from shapely.geometry import Point
+import random
+import requests
 import datetime
-import numpy as np
 
-url = 'https://re.jrc.ec.europa.eu/api/mrcalc'
+def random_point(polygon):
+    # very sparse multipolygons can take long times in theory with simple bounds
+    minx, miny, maxx, maxy = polygon.bounds
+    point = Point()
+    
+    while(not polygon.contains(point)):
+        point = Point(random.uniform(minx, maxx), random.uniform(miny, maxy))
+    
+    return point
 
-for lat in np.linspace(60,70,20):
-  for lon in np.linspace(20,30,20):
 
-    params = {'lat': lat,
-              'lon': lon,
-              'optrad': 1}
+def mean_yearly_total_pv_at_location(point):
+    url = 'https://re.jrc.ec.europa.eu/api/mrcalc'
+    params = {'lat': point.y, 'lon': point.x, 'optrad': 1}
 
     res = requests.get(url, params=params)
 
-    data = res.content.decode('utf-8')
+    data = pd.Series(name='radiation', dtype='float64')
 
-    df = pd.DataFrame(columns=['radiation'])
-
+    # TODO: filter out points in water, maybe combine functions as the sampled points do not know if they are feasible (on land)
     for row in res.content.decode('utf-8').split('\n')[6:-4]:
-      timestamp = pd.Timestamp(datetime.datetime.strptime(row[:9], '%Y\t\t%b'))
-      df.loc[timestamp, 'radiation'] = float(row.split('\t')[-1].split('\r')[0])
+        timestamp = pd.Timestamp(datetime.datetime.strptime(row[:9], '%Y\t\t%b'))
+        data.loc[timestamp] = float(row.split('\t')[-1].split('\r')[0])
 
-    mean = df.mean()
-    print(f'lat: {lat}, lon: {lon}, mean: {mean}')
-# for month, group in df.groupby(df.index.month):
-#   print(f'{month}: {group.mean()}')
+    # yearly_averages = pd.Series(name='yearly average PV', dtype='float64')
+    
+    # for year, group in data.groupby(data.index.year):
+    #     yearly_averages[str(year)] = group.mean()
+
+    return data.groupby(data.index.year).sum().mean()
+
+
+paavo = gpd.read_file('./shapefiles/pno_tilasto_2020.shp')
+data = pd.read_excel('./shapefiles/alueryhmittely_posnro_2020_fi.xlsx', skiprows=4, dtype={'Postinumeroalue': 'str'})
+data['Geometry'] = paavo
+data = data.set_index('Postinumeroalue')                    # read_csv param 'index_col' bugs with dtype
+
+for area_code, area_data in data.iterrows():
+    polygon = area_data['Geometry']
+
+    # Sample 10 points and convert to WGS84, then use PVGIS api to get values for each area code
+    sample_points = gpd.GeoSeries([random_point(polygon) for i in range(10)], crs='EPSG:3067').to_crs('EPSG:4326')
+    sample_pv = pd.Series([mean_yearly_total_pv_at_location(point) for point in sample_points])
+    
+    mean_pv = sample_pv.mean()
+
+    print(f'area code: {area_code}, mean pv: {mean_pv}')
+    data.loc[area_code, 'Mean PV'] = mean_pv
+
+data['Mean PV'].to_csv('results.csv')
